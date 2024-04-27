3aa0704da381ca943ac8c43042b5649b054667e5
TattieMasher <alexandermccaughran@yahoo.com>
2024-04-28 00:37:34 +0100

    Added the settings modal with a search radius, save button and clear data button

--- src/settingsModal.jsx
+++ src/settingsModal.jsx
@@ -7,10 +7,25 @@ import {
     ModalCloseButton,
     ModalBody,
     ModalFooter,
-    Button
+    Button,
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper
   } from '@chakra-ui/react';
   
   export default function SettingsModal({ isOpen, onClose }) {
+    const handleSave = () => {
+        // TODO
+        onClose;
+    }
+
     return (
       <>
         <Modal isOpen={isOpen} onClose={onClose} isCentered>
@@ -19,12 +34,24 @@ import {
             <ModalHeader>Settings</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-              {/* Modal content */}
-              Settings content goes here...
+            <FormControl>
+              <FormLabel>Search radius</FormLabel>
+              <NumberInput defaultValue={5} min={1} max={50}>
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+            <FormHelperText>Distance in miles</FormHelperText>
+            </FormControl>
             </ModalBody>
-            <ModalFooter>
+            <ModalFooter display="flex" justifyContent="space-between">
+              <Button colorScheme="red" onClick={onClose}>
+                Delete stored data
+              </Button>
               <Button colorScheme="blue" mr={3} onClick={onClose}>
-                Close
+                Save
               </Button>
             </ModalFooter>
           </ModalContent>
